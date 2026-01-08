df04017e6350072cbf9c1b86cf9212633e376a66
Pratiksha759 <ppathare759@gmail.com>
2026-01-09 00:01:11 +0530

    Third and todays final PR

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -16,4 +16,6 @@ test('get started link', async ({ page }) => {
     console.log("this is the name ")
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+
+  console.log("Last PR")
 });
